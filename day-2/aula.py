218fb1079b9e3a488bd994c99e8124869911d500
MatheusDLins <matheus934qwer@gmail.com>
2024-05-15 10:25:46 -0300

    class finished

--- day-2/aula.py
+++ day-2/aula.py
@@ -52,4 +52,4 @@ score = 0
 height = 1.8
 isWinning = True
 
-print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+print(f"your score is {score}, your height is {round(height)}, you are winning is {isWinning}")
---
+++ day-8/function_with_inputs.py
@@ -0,0 +1,12 @@
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Matheus")
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with_name("Matheus")
